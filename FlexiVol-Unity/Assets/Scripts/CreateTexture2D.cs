f75a98bad352e53bad4276c0f05176e317db5570
ELODIE LYATH BOUZBIB <elodie.bouzbib@dyc.a.unavarra.es>
2024-07-02 17:32:09 +0200

    slices done, 24 bit planes with colors, merge still to improve

--- FlexiVol-Unity/Assets/Scripts/CreateTexture2D.cs
+++ FlexiVol-Unity/Assets/Scripts/CreateTexture2D.cs
@@ -1,118 +1,73 @@
 namespace OpenCvSharp.Demo
 {
+	using System;
 	using System.Collections;
 	using System.Collections.Generic;
 	using UnityEngine;
 	using OpenCvSharp;
 	using UnityEngine.UI;
 
-
 	public class CreateTexture2D : MonoBehaviour
 	{
+		public int sizeImage = 64;
 		public int showMe;
 		public RenderTexture tex;
-		public Texture2D myTexture;
+		public Texture2D myTexture, reMergedTexture;
 		public RawImage rawImage;
-		// public Texture2D[] bitPlaneTexturesR, bitPlaneTexturesG, bitPlaneTexturesB;
+		public Texture2D[] bitPlaneTextures; //, bitPlaneTexturesG, bitPlaneTexturesB;
 		// public Color32[] pixelsR;//, pixelsG, pixelsB;
 		private Mat[] testMatrix;
 		public Texture2D[] texturedPlanes;
+		// public float[,] dataR;
+		
+		[Serializable]
+		public struct bitPlaneInfo
+		{
+			public int bitplaneID;
+			public Color32[] pixels;
+		}
+
+		public bitPlaneInfo[] bitplanes;
+
 
 	    // Start is called before the first frame update
 	    void Start()
 	    {
-	    	// myTexture = toTexture2D(tex);
-
-	        // Mat mat = Unity.TextureToMat (myTexture);
+	    	
 	        testMatrix = new Mat[3];
-	        texturedPlanes = new Texture2D[3];
-	    	// bitPlaneTexturesR = new Texture2D[8];
-	        // bitPlaneTexturesG = new Texture2D[8];
-	        // bitPlaneTexturesB = new Texture2D[8];
+	        texturedPlanes = new Texture2D[3]; // 0 is B, 1 is G, 2 is R
 
-	        // pixelsR = new Color32[myTexture.GetPixels32(0).Length];
-	        // pixelsG = new Color32[128];//[myTexture.GetPixels32(0).Length];
-	        // pixelsB = new Color32[128];//[myTexture.GetPixels32(0).Length];
-	        
-	    	// for(int i = 0; i < 8; i++)
-	    	// {
-	    	// 	bitPlaneTexturesR[i] = (Texture2D)Instantiate(myTexture, this.transform);
-	    	// 	// GameObject newPlanes = (GameObject)Instantiate(this.gameObject, this.transform);
-	    	// 	// newPlanes.GetComponent<MeshRenderer>().material.SetTexture("Tarace", bitPlaneTexturesR[i]);
-	    	// 	// bitPlaneTexturesG[i] = (Texture2D)Instantiate(myTexture, this.transform);
-	    	// 	// bitPlaneTexturesB[i] = (Texture2D)Instantiate(myTexture, this.transform);
+	        bitplanes = new bitPlaneInfo[24];
+	    	bitPlaneTextures = new Texture2D[24];
 
-	    	// }
+	    	for(int j = 0; j < 8; j++)
+			{
+				bitplanes[j].bitplaneID = j;
+				bitplanes[j].pixels = new Color32[sizeImage*sizeImage];
+
+				bitplanes[j+8].bitplaneID = j+8;
+				bitplanes[j+8].pixels = new Color32[sizeImage*sizeImage];
+
+				bitplanes[j+16].bitplaneID = j+16;
+				bitplanes[j+16].pixels = new Color32[sizeImage*sizeImage];
+			}
+
 	        
 	    }
 
 	    // Update is called once per frame
 	    void Update()
 	    {
-	        myTexture = toTexture2D(tex);
-
-	        Mat mat = Unity.TextureToMat (myTexture);
-			// Mat grayMat = new Mat ();
-			// Cv2.CvtColor (mat, grayMat, ColorConversionCodes.BGR2GRAY); 
-			// Texture2D texture = Unity.MatToTexture (grayMat);
-			// texture.Apply();
-
-			testMatrix = Cv2.Split(mat);
-
-			for(int i = 0; i < 3; i++)
-			{
-				texturedPlanes[i] = Unity.MatToTexture(testMatrix[i]);
-			}
-			Texture2D texture = Unity.MatToTexture (testMatrix[showMe]);
-			texture.Apply();
-
-			// Use GetPixelData to get an array that points to mipmap level 1
-	        // var mip1Data = myTexture.GetPixelData<Color32>(0);
-	        // Debug.Log(mip1Data.Length);
-
-
-                // Utils.matToTexture2D(rgbMat, texture);
-	        // Color32[] pixelsTexture = myTexture.GetPixels32(0);
-
-	        // System.Array.SetValue(Mathf.Floor(pixelsTexture.r/ Mathf.Pow(2,j)) % 2);
-
-	        
-        	// for(int j = 0; j < 8; j++)
-        	// {
-        	// 	for(int i = 0; i < mip1Data.Length; i++)
-	        // 	{
-	        // 		// var dataR = Mathf.Floor(pixelsTexture[i].r/ Mathf.Pow(2,j)) % 2;
-	        // 		// var dataG = Mathf.Floor(pixelsTexture[i].g/ Mathf.Pow(2,j)) % 2;
-	        // 		// var dataB = Mathf.Floor(pixelsTexture[i].b/ Mathf.Pow(2,j)) % 2;
-
-	        // 		pixelsR[i] = new Color32((byte)(Mathf.Floor(mip1Data[i].r/Mathf.Pow(2,(j+1))) % 2), 0, 0, 1);
-	        // 		// pixelsG[i] = new Color32(0, (byte)(Mathf.Floor(pixelsTexture[i].g/Mathf.Pow(2,(j+1))) % 2), 0, 1);
-	        // 		// pixelsB[i] = new Color32(0, 0, (byte)(Mathf.Floor(pixelsTexture[i].b/Mathf.Pow(2,(j+1))) % 2), 1);
-
-		       //  	// bitPlaneTexturesR[j].SetPixels(i, Mathf.Floor(pixelsTexture[i].r/ Mathf.Pow(2,j)) % 2);
-		       //  	// bitPlaneTexturesG[j].SetPixel(i, Mathf.Floor(pixelsTexture[i].g/ Mathf.Pow(2,j)) % 2);
-		       //  	// bitPlaneTexturesB[j].SetPixel(i, Mathf.Floor(pixelsTexture[i].b/ Mathf.Pow(2,j)) % 2);
-
-	        // 	}
-
-	        // 	// bitPlaneTexturesR[j].SetPixels32(pixelsR,0);
-	        // 	// bitPlaneTexturesG[j].SetPixels32(pixelsG,0);
-	        // 	// bitPlaneTexturesB[j].SetPixels32(pixelsB,0);
-
-	        // 	// bitPlaneTexturesR[j].Apply();
-	        // 	// bitPlaneTexturesG[j].Apply();
-	        // 	// bitPlaneTexturesB[j].Apply();
-
-	        // }
-
-			rawImage.texture = texture;
-			// rawImage.texture = bitPlaneTexturesR[0];
+	        if(Input.GetKeyDown(KeyCode.Space))
+	        {
+	        	StartCoroutine(GetTheTextureStuff());
+	        }
 
 	    }
 
 	    Texture2D toTexture2D(RenderTexture rTex)
 		{
-		    Texture2D tex = new Texture2D(512, 512, TextureFormat.RGB24, false);
+		    Texture2D tex = new Texture2D(64, 64, TextureFormat.RGB24, false);
 		    // ReadPixels looks at the active RenderTexture.
 		    RenderTexture.active = rTex;
 		    tex.ReadPixels(new UnityEngine.Rect(0, 0, rTex.width, rTex.height), 0, 0);
@@ -121,7 +76,89 @@ namespace OpenCvSharp.Demo
 		    return tex;
 		}
 
-		
+		IEnumerator GetTheTextureStuff()
+		{
+			yield return new WaitForEndOfFrame();
+
+			myTexture = toTexture2D(tex);
+
+	        Mat mat = Unity.TextureToMat(myTexture);
+			testMatrix = Cv2.Split(mat);
+
+
+			for(int i = 0; i < 3; i++)
+			{
+				texturedPlanes[i] = Unity.MatToTexture(testMatrix[i]);
+
+			}
+
+			// Debug.Log(testMatrix[0]);
+			Texture2D texture = Unity.MatToTexture(testMatrix[showMe]);
+			// texture.Apply();
+
+			// rawImage.texture = texture;
+			StartCoroutine(CalculateBitPlane());
+
+		}
+
+		IEnumerator CalculateBitPlane()
+		{
+			yield return new WaitForEndOfFrame();
+
+			Color32[] pixels = myTexture.GetPixels32(0);
+
+			for(int j = 0; j < 8; j++)
+			{
+				for(int i = 0; i < pixels.Length; i++)
+				{
+					bitplanes[j].pixels[i] = new Color32((byte)((Mathf.Floor(pixels[i].r / Mathf.Pow(2,(j+1))) % 2)*255), 0, 0, 255);
+					bitplanes[j+8].pixels[i] = new Color32(0, (byte)((Mathf.Floor(pixels[i].g / Mathf.Pow(2,(j+1))) % 2)*255), 0, 255);
+					bitplanes[j+16].pixels[i] = new Color32(0, 0, (byte)((Mathf.Floor(pixels[i].b / Mathf.Pow(2,(j+1))) % 2)*255), 255);
+				}
+			}
+			for(int j = 0; j < 24; j++)
+			{
+				bitPlaneTextures[j].SetPixels32(bitplanes[j].pixels, 0);
+			}
+
+			StartCoroutine(CreateBinaryPlanes());
+
+		}
+
+		IEnumerator CreateBinaryPlanes() // Now we need to get each bitplane pixel and ID and make it into a texture, then merge the texture
+		{
+			yield return new WaitForSeconds(3);
+			// Merge is B G R order
+			// for(int i = 23; i > -1; i--)
+			// {
+			Mat[] newmat = new Mat[24];
+			for(int i = 0; i < 24; i++)
+			{
+				newmat[i] = Unity.TextureToMat(bitPlaneTextures[i]);
+			}
+			Mat materialTest = new Mat();
+			Cv2.Merge(newmat, materialTest);
+			// }
+			
+			reMergedTexture = Unity.MatToTexture(materialTest);
+			reMergedTexture.Apply();
+			rawImage.texture = reMergedTexture;
+			// fullImage = (2 * bitplanes[7].pixels);
+
+			// for(int j = 7; j > 0; j++)
+			// {
+			// 	fullImage += bitplanes[j-1].pixels;
+			// 	fullImage *= 2;
+			// 	// bitPlaneTexturesR[j].SetPixels32(new Color32((byte)bitplanes[j].pixels, 0,0,1),0);
+			// }
+			// for(int j = 15; j > 8; j++)
+			// {
+			// 	fullImage += bitplanes[j-1].pixels;
+			// 	fullImage *= 2;
+			// 	// bitPlaneTexturesR[j].SetPixels32(new Color32((byte)bitplanes[j].pixels, 0,0,1),0);
+			// }
+			
+		}
 
 
 	}
